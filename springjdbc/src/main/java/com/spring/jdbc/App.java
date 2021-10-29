ce2f61855099dc132bf6e5a60e9466d547f0bf22
Rishabh Bansal <rishabh.bansal2200@gmail.com>
2021-10-29 14:58:06 +0530

    added config in xml and insert firts data

--- springjdbc/src/main/java/com/spring/jdbc/App.java
+++ springjdbc/src/main/java/com/spring/jdbc/App.java
@@ -1,13 +1,22 @@
 package com.spring.jdbc;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
+		System.out.println("bansal");
+		JdbcTemplate template = context.getBean("JdbcTemplate", JdbcTemplate.class);
+		System.out.println(template);
+		System.out.println("rishabh");
+		String query = "insert into springjdbc(id,studentName,studentAddress) values(?,?,?)";
+		int result =template.update(query,246,"bansal","334");
+		
+	}
 }
